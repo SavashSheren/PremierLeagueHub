16a7743f8e0f29862e15c20e8ebc86cceaf896fb
SAVAS SEREN <sava.se6565@gmail.com>
2026-06-01 06:23:14 +0300

    Add admin team list page

---
+++ PremierLeagueHub.WebUI/Areas/Admin/Controllers/AdminTeamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PremierLeagueHub.DtoLayer.TeamDtos;
+using System.Net.Http.Json;
+
+namespace PremierLeagueHub.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class AdminTeamController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AdminTeamController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient("PremierLeagueApi");
+
+        var teams = await client.GetFromJsonAsync<List<ResultTeamDto>>("Teams");
+
+        return View(teams ?? new List<ResultTeamDto>());
+    }
+}
--- PremierLeagueHub.WebUI/Program.cs
+++ PremierLeagueHub.WebUI/Program.cs
@@ -1,15 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient("PremierLeagueApi", client =>
+{
+    var baseUrl = builder.Configuration["ApiSettings:BaseUrl"];
+
+    if (string.IsNullOrWhiteSpace(baseUrl))
+    {
+        throw new InvalidOperationException("API base URL is not configured.");
+    }
+
+    client.BaseAddress = new Uri(baseUrl);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -19,6 +28,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
