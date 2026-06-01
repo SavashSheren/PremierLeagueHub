8b23c29f05485b1b543df0c85f1ccc6826077fbb
SAVAS SEREN <sava.se6565@gmail.com>
2026-06-01 07:34:36 +0300

    Add admin dashboard page

---
+++ PremierLeagueHub.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PremierLeagueHub.DtoLayer.TeamDtos;
+using PremierLeagueHub.WebUI.Models;
+using System.Net.Http.Json;
+
+namespace PremierLeagueHub.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class DashboardController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public DashboardController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient("PremierLeagueApi");
+
+        var teams = await client.GetFromJsonAsync<List<ResultTeamDto>>("Teams")
+                    ?? new List<ResultTeamDto>();
+
+        var model = new AdminDashboardViewModel
+        {
+            TotalTeams = teams.Count,
+            ActiveTeams = teams.Count(x => x.IsActive),
+            PassiveTeams = teams.Count(x => !x.IsActive),
+            TotalCities = teams.Select(x => x.City).Distinct().Count(),
+            LatestTeams = teams
+                .OrderByDescending(x => x.TeamId)
+                .Take(5)
+                .ToList()
+        };
+
+        return View(model);
+    }
+}
---
+++ PremierLeagueHub.WebUI/Models/AdminDashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PremierLeagueHub.DtoLayer.TeamDtos;
+
+namespace PremierLeagueHub.WebUI.Models;
+
+public class AdminDashboardViewModel
+{
+    public int TotalTeams { get; set; }
+    public int ActiveTeams { get; set; }
+    public int PassiveTeams { get; set; }
+    public int TotalCities { get; set; }
+
+    public List<ResultTeamDto> LatestTeams { get; set; } = new List<ResultTeamDto>();
+}
