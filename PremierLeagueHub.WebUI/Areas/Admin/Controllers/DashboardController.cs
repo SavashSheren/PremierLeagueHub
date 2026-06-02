b31bd239b66df41c60ee1dd10ac7c0024fbeb2f7
SAVAS SEREN <sava.se6565@gmail.com>
2026-06-02 05:59:06 +0300

    Upgrade admin dashboard analytic

--- PremierLeagueHub.WebUI/Areas/Admin/Controllers/DashboardController.cs
+++ PremierLeagueHub.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PremierLeagueHub.DtoLayer.FixtureDtos;
+using PremierLeagueHub.DtoLayer.MatchEventDtos;
 using PremierLeagueHub.DtoLayer.TeamDtos;
 using PremierLeagueHub.WebUI.Models;
 using System.Net.Http.Json;
@@ -19,21 +21,62 @@ public class DashboardController : Controller
     {
         var client = _httpClientFactory.CreateClient("PremierLeagueApi");
 
-        var teams = await client.GetFromJsonAsync<List<ResultTeamDto>>("Teams")
-                    ?? new List<ResultTeamDto>();
-
-        var model = new AdminDashboardViewModel
+        try
         {
-            TotalTeams = teams.Count,
-            ActiveTeams = teams.Count(x => x.IsActive),
-            PassiveTeams = teams.Count(x => !x.IsActive),
-            TotalCities = teams.Select(x => x.City).Distinct().Count(),
-            LatestTeams = teams
-                .OrderByDescending(x => x.TeamId)
-                .Take(5)
-                .ToList()
-        };
+            var teams = await client.GetFromJsonAsync<List<ResultTeamDto>>("Teams")
+                        ?? new List<ResultTeamDto>();
 
-        return View(model);
+            var fixtures = await client.GetFromJsonAsync<List<ResultFixtureDto>>("Fixtures")
+                           ?? new List<ResultFixtureDto>();
+
+            var matchEvents = await client.GetFromJsonAsync<List<ResultMatchEventDto>>("MatchEvents")
+                              ?? new List<ResultMatchEventDto>();
+
+            var model = new AdminDashboardViewModel
+            {
+                TotalTeams = teams.Count,
+                ActiveTeams = teams.Count(x => x.IsActive),
+                PassiveTeams = teams.Count(x => !x.IsActive),
+                TotalCities = teams
+                    .Select(x => x.City)
+                    .Where(x => !string.IsNullOrWhiteSpace(x))
+                    .Distinct()
+                    .Count(),
+
+                TotalFixtures = fixtures.Count,
+                FinishedFixtures = fixtures.Count(x => x.Status == "Finished" || x.HasResult),
+                ScheduledFixtures = fixtures.Count(x => x.Status == "Scheduled" && !x.HasResult),
+                LiveFixtures = fixtures.Count(x => x.Status == "Live"),
+                TopMatchCount = fixtures.Count(x => x.IsTopMatch),
+
+                TotalMatchEvents = matchEvents.Count,
+                GoalEvents = matchEvents.Count(x =>
+                    x.EventType == "Goal" || x.EventType == "Penalty"),
+                CardEvents = matchEvents.Count(x =>
+                    x.EventType == "YellowCard" || x.EventType == "RedCard"),
+
+                LatestTeams = teams
+                    .OrderByDescending(x => x.TeamId)
+                    .Take(5)
+                    .ToList(),
+
+                LatestFixtures = fixtures
+                    .OrderByDescending(x => x.MatchDate)
+                    .Take(5)
+                    .ToList(),
+
+                LatestMatchEvents = matchEvents
+                    .OrderByDescending(x => x.MatchEventId)
+                    .Take(6)
+                    .ToList()
+            };
+
+            return View(model);
+        }
+        catch
+        {
+            ViewBag.ApiError = "Dashboard data could not be loaded. Please check WebApi service.";
+            return View(new AdminDashboardViewModel());
+        }
     }
 }
--- PremierLeagueHub.WebUI/Models/AdminDashboardViewModel.cs
+++ PremierLeagueHub.WebUI/Models/AdminDashboardViewModel.cs
@@ -1,4 +1,6 @@
-﻿using PremierLeagueHub.DtoLayer.TeamDtos;
+﻿using PremierLeagueHub.DtoLayer.FixtureDtos;
+using PremierLeagueHub.DtoLayer.MatchEventDtos;
+using PremierLeagueHub.DtoLayer.TeamDtos;
 
 namespace PremierLeagueHub.WebUI.Models;
 
@@ -9,5 +11,17 @@ public class AdminDashboardViewModel
     public int PassiveTeams { get; set; }
     public int TotalCities { get; set; }
 
+    public int TotalFixtures { get; set; }
+    public int FinishedFixtures { get; set; }
+    public int ScheduledFixtures { get; set; }
+    public int LiveFixtures { get; set; }
+    public int TopMatchCount { get; set; }
+
+    public int TotalMatchEvents { get; set; }
+    public int GoalEvents { get; set; }
+    public int CardEvents { get; set; }
+
     public List<ResultTeamDto> LatestTeams { get; set; } = new List<ResultTeamDto>();
+    public List<ResultFixtureDto> LatestFixtures { get; set; } = new List<ResultFixtureDto>();
+    public List<ResultMatchEventDto> LatestMatchEvents { get; set; } = new List<ResultMatchEventDto>();
 }
